adcdbb03a4c5295f899177d270db8cda779c3174
edsarmiento1 <edsarmiento1@umsa.bo>
2023-03-13 18:30:24 -0400

    Terminando la clase 60

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 140000
+  }
+  agregar( ){
+    console.log('Esta es una prueba');
   }
 
+  cambiarNombre(event: any){
+    console.log(event.target.value);
+  }
+
+  // agregar( event: any){
+  //   event.preventDefault();
+  //   console.log('Hey...!!!');
+  // }
 }
