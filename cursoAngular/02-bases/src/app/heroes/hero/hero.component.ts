f7ceb15bac068d63ff62eaf422e4ad714cdbb848
edsarmiento1 <edsarmiento1@umsa.bo>
2023-09-27 14:37:37 -0400

    Terminando hasta la clase 42

---
+++ cursoAngular/02-bases/src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+  public name: string = 'IronMan';
+  public age: number = 45;
+  
+}
