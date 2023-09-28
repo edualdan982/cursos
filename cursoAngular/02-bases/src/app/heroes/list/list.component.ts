8e8be69c4ffcc4a8e2d4f2239ccb77dd3dd1450e
edualdan982 <wipitor982@gmail.com>
2023-09-28 01:19:28 -0400

    Terminando la sección 4 de Angular.

--- cursoAngular/02-bases/src/app/heroes/list/list.component.ts
+++ cursoAngular/02-bases/src/app/heroes/list/list.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
+  public herosNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Shehulk',
+  ];
 
+  public deleteHero?: string;
+  removeLastHero(): void {
+    this.deleteHero = this.herosNames.pop();
+  }
 }
