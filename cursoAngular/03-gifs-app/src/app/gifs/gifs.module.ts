f9182c68992b37817924ea12ae2151061e911036
edualdan982 <wipitor982@gmail.com>
2023-10-05 01:05:41 -0400

    Termianndo hasta la clase 87

--- cursoAngular/03-gifs-app/src/app/gifs/gifs.module.ts
+++ cursoAngular/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ShearchBoxComponent } from './components/search-box/shearch-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent, ShearchBoxComponent, CardListComponent],
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
