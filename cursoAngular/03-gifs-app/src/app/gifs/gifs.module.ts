2d0ed5138fd314c0381af12b54fa2d467936ee57
edualdan982 <wipitor982@gmail.com>
2023-10-08 21:27:50 -0400

    Terminando la sección 8 hasta la clase 107. Además se configura la aplicación de 03

--- cursoAngular/03-gifs-app/src/app/gifs/gifs.module.ts
+++ cursoAngular/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+import { CardComponent } from './components/card/card.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ShearchBoxComponent } from './components/search-box/shearch-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, ShearchBoxComponent, CardListComponent],
-  imports: [CommonModule],
+  declarations: [
+    HomePageComponent,
+    ShearchBoxComponent,
+    CardListComponent,
+    CardComponent,
+  ],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
