13f085fd11b487c5f9f69565086a66e45a25659c
edsarmiento1 <edsarmiento1@umsa.bo>
2023-10-24 18:12:48 -0400

    Copia de seguridad.

--- cursoAngular/03-gifs-app/src/app/app.module.ts
+++ cursoAngular/03-gifs-app/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ],
   imports: [BrowserModule, GifsModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
