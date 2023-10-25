201a28f83d9ef2636eadb0b30cda3a81d15d740f
edualdan982 <wipitor982@gmail.com>
2023-10-25 00:10:05 -0400

    Terminmando hasta la clase 118

--- cursoAngular/04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ cursoAngular/04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCapitalPageComponent {
-
+  searchByCapital(term: string) {
+    console.log('Desde el ByCapitalPage');
+    console.log(`Term: ${term}`);
+  }
 }
--- cursoAngular/04-country-app/src/app/shared/shared.module.ts
+++ cursoAngular/04-country-app/src/app/shared/shared.module.ts
@@ -6,12 +6,14 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 @NgModule({
   declarations: [
     AboutPageComponent,
     ContactPageComponent,
     HomePageComponent,
     SidebarComponent,
+    SearchBoxComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
@@ -19,6 +21,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     ContactPageComponent,
     HomePageComponent,
     SidebarComponent,
+    SearchBoxComponent,
   ],
 })
 export class SharedModule {}
