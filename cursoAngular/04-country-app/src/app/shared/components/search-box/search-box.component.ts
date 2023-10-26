c22344cbd247f2db0d4516993c7a11074f3a3643
edualdan982 <wipitor982@gmail.com>
2023-10-26 00:06:34 -0400

    Terminando hasta la clase 120

--- cursoAngular/04-country-app/src/app/shared/components/search-box/search-box.component.ts
+++ cursoAngular/04-country-app/src/app/shared/components/search-box/search-box.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SearchBoxComponent {
   @Input()
-  public placeholder: string='';
+  public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  @ViewChild('txtValue')
+  public value!: ElementRef<HTMLInputElement>;
+
+  emitValue(): void {
+    const newValue = this.value.nativeElement.value;
+    console.log('Nuevo valor: ' + newValue);
+
+    this.onValue.emit(newValue);
+    // this.value.nativeElement.value = '';
+  }
 }
