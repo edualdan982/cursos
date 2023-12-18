ce96b93a08e120115f2bd45a1ca64536347c87ea
edsarmiento1 <edsarmiento1@umsa.bo>
2023-12-18 18:56:36 -0400

    Se termino la clase parcial de i18nSelect.

--- cursoAngular/05-pipe-app/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ cursoAngular/05-pipe-app/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./numbers-page.component.css']
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 2546213.1646;
+  public percent: number = 0.4568;
+  
 }
--- cursoAngular/05-pipe-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ cursoAngular/05-pipe-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Edual Dan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melisa';
+    this.gender = 'female';
+  }
+  
+  // i18n Plural
+  public clients: string[] = [ 'Maria', 'Pedro', 'Juan', 'Natalia', 'Fernando', 'Roberto' ];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando.'
+  }
+  deleteClient(): void{
+    this.clients.shift();
+  }
 }
