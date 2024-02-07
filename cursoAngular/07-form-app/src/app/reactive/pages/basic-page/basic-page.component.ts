b17f8fa42c724a9139a6c7df66d3230bbe2510b8
edualdan982 <wipitor982@gmail.com>
2024-02-07 01:21:58 -0400

    Formgroup

--- cursoAngular/07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ cursoAngular/07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicPageComponent {
+  //Este es un forma de un FormGroup
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0)
+  // });
 
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
+    ],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    if(this.myForm.invalid) return;
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 }
--- cursoAngular/07-form-app/src/app/reactive/reactive.module.ts
+++ cursoAngular/07-form-app/src/app/reactive/reactive.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { ReactiveRoutingModule } from './reactive-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveRoutingModule } from './reactive-routing.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
