75a76e8ba75bccdcd4a4480f498c66377541c752
edualdan982 <wipitor982@gmail.com>
2024-02-08 00:30:43 -0400

    Terminando los formularios con validadores

--- cursoAngular/07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ cursoAngular/07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+};
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   //Este es un forma de un FormGroup
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -23,11 +28,41 @@ export class BasicPageComponent {
   });
 
   constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors ?? {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'El campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors[key].requiredLength} caracteres.`;
+        case 'min':
+          return `Debe ser mayor o igual a ${errors[key].min}`;
+      }
+      return null;
+    }
+    return 'Hola edual';
+  }
 
   onSave(): void {
-    if(this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value);
 
-    this.myForm.reset();
+    this.myForm.reset({ price: 10, inStorage: 0 });
   }
 }
