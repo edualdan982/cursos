c374936a8b86e915ba2d6eabdbdd7d14160bafc6
edualdan982 <wipitor982@gmail.com>
2024-03-08 00:32:16 -0400

    Terminando hasta la clase 294

--- cursoAngular/10-maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ cursoAngular/10-maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-68.15, -16.5], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+    this.mapListener();
+  }
+
+  mapListener(){
+    if(!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) =>{
+      this.zoom = this.map!.getZoom();
+
+    });
+    this.map.on('zoomend', (ev) =>{
+      if(this.map!.getZoom() <= 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 }
