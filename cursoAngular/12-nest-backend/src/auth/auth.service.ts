ba0aa0f94ba238f5682654c52e8af92b8b820228
edualdan982 <wipitor982@gmail.com>
2024-03-12 01:04:09 -0400

    Terminando hasta el 344

--- cursoAngular/12-nest-backend/src/auth/auth.service.ts
+++ cursoAngular/12-nest-backend/src/auth/auth.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/auth.entity';
+import { Model } from 'mongoose';
+import { Type } from '../../../03-gifs-app/src/app/gifs/interfaces/gifs.interface';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // 1. Encriptar el password
+    // 2. Guardar el usuario
+    // 3. Generar JWT
+
+    try {
+      const newUser = new this.userModel(createUserDto);
+      return await newUser.save();
+    } catch (error: any) {
+      console.log("Validanddo");
+      console.log(error.code === '11000');
+      if(error.code && Number(error.code) === 11000){
+        throw new BadRequestException(`${createUserDto.email} ya existe`)
+      }
+      throw new InternalServerErrorException(`Error en el proceso de save.`)
+    }
   }
 
   findAll() {
