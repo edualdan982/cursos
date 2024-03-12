2ed4e7f7a1bec700407ddb31f5a137b933ab04a1
edualdan982 <wipitor982@gmail.com>
2024-03-11 21:36:18 -0400

    Agregando los cambios de nest-backend del curso, que se habia creado como modulo.

---
+++ cursoAngular/12-nest-backend/src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User, UserSchema } from './entities/auth.entity';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+})
+export class AuthModule {}
