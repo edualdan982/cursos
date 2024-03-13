add4405c69400592b21088e63b8dfb6b6d07afca
edualdan982 <wipitor982@gmail.com>
2024-03-13 00:58:15 -0400

    Falta la tarea 354, pero se termino hasta la clase 353.

--- cursoAngular/12-nest-backend/src/auth/auth.controller.ts
+++ cursoAngular/12-nest-backend/src/auth/auth.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login-dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,23 +25,39 @@ export class AuthController {
     return this.authService.create(createUserDto);
   }
 
+  @Post('login')
+  login(@Body() login: LoginDto) {
+    return this.authService.login(login);
+  }
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
+  }
+
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() req: Request) {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
+
+  @Post('check-token')
+  checkToken(@Request() req: Request) {
+    return this.authService.checkToken(req);
   }
 }
--- cursoAngular/12-nest-backend/src/auth/auth.service.ts
+++ cursoAngular/12-nest-backend/src/auth/auth.service.ts
@@ -1,34 +1,75 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/auth.entity';
 import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+
+import { LoginDto, CreateUserDto, RegisterUserDto, UpdateAuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    // 1. Encriptar el password
-    // 2. Guardar el usuario
-    // 3. Generar JWT
-
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
+      const { password, ...useData } = createUserDto;
+      // 1. Encriptar el password
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...useData,
+      });
+      // 2. Guardar el usuario
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
     } catch (error) {
-      if(Number(error['code']) === 11000){
-        throw new BadRequestException(`${createUserDto.email} ya existe`)
+      if (Number(error['code']) === 11000) {
+        throw new BadRequestException(`${createUserDto.email} ya existe`);
       }
-      throw new InternalServerErrorException(`Error en el proceso de save.`)
+      throw new InternalServerErrorException(`Error en el proceso de save.`);
     }
   }
+  async register(registerUserDto: RegisterUserDto): Promise<LoginResponse> {
+    const { password, confirmPassword, ...userData } = registerUserDto;
+    if (password !== confirmPassword)
+      throw new BadRequestException(`Las claves no coinciden`);
 
-  findAll() {
-    return `This action returns all auth`;
+    await this.create({ password, ...userData });
+    return this.login({ password, email: userData.email });
+  }
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+    if (!user || !bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credenciales no validas');
+    }
+    const { password: _, ...rest } = user.toJSON();
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id }),
+    };
   }
 
+  async findAll(): Promise<User[]> {
+    return await this.userModel.find();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel.findById(id);
+    const { password, ...rest } = user.toJSON();
+    return rest;
+  }
   findOne(id: number) {
     return `This action returns a #${id} auth`;
   }
@@ -39,5 +80,31 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+
+  async checkToken(req: Request): Promise<JwtPayload> {
+    const token = this.extractTokenFromHeader(req);
+    if (!token) {
+      throw new UnauthorizedException(
+        'Se espera un token en la cabecera Authorization: Bearer JWT',
+      );
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      
+    } catch (error) {
+      
+    }
+    return;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
