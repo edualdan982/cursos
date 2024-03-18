ba3d5739638352a554d74b9a8a51e0cc3c49c17c
edsarmiento1 <edsarmiento1@umsa.bo>
2024-03-18 15:10:48 -0400

    Agregando el formulario de registro de usuario.

--- cursoAngular/13-auth-app/src/app/auth/pages/register-page/register-page.component.ts
+++ cursoAngular/13-auth-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../services/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
 export class RegisterPageComponent {
+  private authService = inject(AuthService);
+  private validatorsService = inject(ValidatorsService);
+  private fb = inject(FormBuilder);
 
+  public myForm = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.pattern(this.validatorsService.emailPattern)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo(
+          'password',
+          'confirmPassword'
+        ),
+      ],
+    }
+  );
+  constructor() {}
+
+  register() {
+    console.log(this.myForm.value);
+
+  }
 }
