ba9e715ee99f0d8600e82e00f1d071ca810f4d28
edualdan982 <wipitor982@gmail.com>
2024-04-17 00:36:21 -0400

    Respaldando el codigo fuente.

--- cursoAngular/my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ cursoAngular/my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal, computed } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [title]="currentFramework()" />
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
+  `,
+  styles: ``,
 })
 export default class ChangeDetectionComponent {
+  public currentFramework =  computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2017,
+  };
 
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      // ? Una forma de asignar y cambiar los datos del valor de la señal
+      /*this.frameworkAsSignal.update( value => ({
+        ...value, name: 'React'
+      }));*/
+      this.frameworkAsSignal.update( value => {
+        value.name = 'React';
+        return {...value}
+      });
+      console.log('Hecho');
+      console.log(`Propiedad cambiada: ${this.frameworkAsProperty.name}`);
+    }, 3000);
+  }
 }
