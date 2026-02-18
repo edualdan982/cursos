e94022cf46d512b43f7e3ee10e40a2a06705b6c7
edsarmiento1 <edsarmiento1@umsa.bo>
2026-02-18 16:20:59 -0400

    add: Agregar las desusturación de objetos.

---
+++ react/v2/01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -0,0 +1,41 @@
+const person = {
+  name: "Tony",
+  age: 45,
+  key: "IronMan",
+};
+
+const { key, name: ironmanName, age } = person;
+
+// const name = person.name
+// const age = person.age
+// const key = person.key
+
+console.log({ key, ironmanName, age });
+
+interface Hero {
+  name: string;
+  age: number;
+  key: string;
+  rank?: string;
+}
+
+const useContext = ({ key, name, age, rank = "sin rango" }: Hero) => {
+  return {
+    keyName: key,
+    user: {
+      name,
+      age,
+    },
+    rank: rank,
+  };
+};
+
+const {
+  rank,
+  keyName,
+  user,
+  //   user: { name },
+} = useContext(person);
+
+const { name } = user;
+console.log({ rank, keyName, name });
--- react/v2/01-reforzamiento/src/main.ts
+++ react/v2/01-reforzamiento/src/main.ts
@@ -1,11 +1,12 @@
-import './style.css'
+import "./style.css";
 
 // import './bases/03-object-literal';
 // import './bases/04-arrays';
-import './bases/pruebas';
+import "./bases/06-obj-destructuring";
+// import './bases/pruebas';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Hola mundo</h1>
   </div>
-`
+`;
