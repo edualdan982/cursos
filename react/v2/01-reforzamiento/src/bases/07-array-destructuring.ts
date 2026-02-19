effc0c212d7dd10addaf540a1c2a730d910ae095
edsarmiento1 <edsarmiento1@umsa.bo>
2026-02-19 16:53:11 -0400

    end: Terminar la clase de interfaces y enumeraciones.

--- react/v2/01-reforzamiento/src/bases/07-array-destructuring.ts
+++ react/v2/01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -12,3 +12,17 @@ const [letters, numbers] = returnsArrayFn();
 
 
 console.log({ letters, numbers });
+
+
+const useState= (value: string) => {
+  return [
+    value,
+    (newValue: string) =>{
+      console.log(`newValue: ${newValue}`);
+    }
+  ] as const
+}
+
+const [name, setName] = useState('Goku');
+console.log(name);       // Goku
+setName('Vegeta');       // Imprime "Vegeta"
--- react/v2/01-reforzamiento/src/main.ts
+++ react/v2/01-reforzamiento/src/main.ts
@@ -3,7 +3,8 @@ import "./style.css";
 // import './bases/03-object-literal';
 // import './bases/04-arrays';
 // import "./bases/06-obj-destructuring";
-import "./bases/07-array-destructuring";
+// import "./bases/07-array-destructuring";
+import "./bases/07-tarea";
 // import './bases/pruebas';
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
