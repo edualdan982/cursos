2508d407ccb119ebb9ff56e40ff0935516683be0
Edual Dan <wipitor982@gmail.com>
2026-02-24 23:37:58 -0400

    add: Terminar la sección 4

--- react/v2/02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ react/v2/02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,35 +1,39 @@
-import { type CSSProperties } from "react";
+import { useState } from "react";
+// import "./ItemCounter.css";
+import styles from './ItemCounter.module.css';
 
 interface Props {
   name: string;
   quantity?: number;
 }
 
-const sectionStyles: CSSProperties = {
-  display: "flex",
-  alignItems: "center",
-  gap: 10,
-  marginTop: 10,
-};
+// const sectionStyles: CSSProperties = {
+//   display: "flex",
+//   alignItems: "center",
+//   gap: 10,
+//   marginTop: 10,
+// };
 
-const spanStyles: CSSProperties = {
-  width: 150,
-};
-export const ItemCounter = ({ name, quantity }: Props) => {
-  const handleClick = () => {
-    console.log(event);
-    console.log(`Click ${name}`);
+// const spanStyles: CSSProperties = {
+//   width: 150,
+// };
+export const ItemCounter = ({ name, quantity = 0 }: Props) => {
+  const [count, setCount] = useState<number>(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    if (count === 0) return;
+    setCount(count - 1);
   };
   return (
-    <section style={sectionStyles}>
-      <span style={spanStyles}>{name}</span>
-      <button
-        onClick={() => handleClick()}
-      >
-        +1
-      </button>
-      <span>{quantity}</span>
-      <button>-1</button>
+    <section className={styles.itemRow}>
+      <span className={styles.itemText} style={{ color: count === 1 ? "red" : "black" }}>{name}</span>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button className={styles['color-red']} onClick={handleSubtract}>-1</button>
     </section>
   );
 };
