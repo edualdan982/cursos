c21a95c6ec17a644af988db04df2b84ca23576c7
edual <wipitor982@gmail.com>
2026-03-12 12:01:06 -0400

    add: Terminar la clase de desestrucutración con arreglos y funciones

---
+++ curso-angular/v2/01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,46 @@
+export interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Samsung S20fe',
+    price: 3200
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 4500
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+// function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+// function taxCalculation({ tax, products }: TaxCalculationOptions ): [number, number] {
+export function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+
+    const { tax, products } = options;
+
+    let total = 0;
+
+    products.forEach( ({ price }) => {
+        total += price;
+    });
+
+    return [total, total * tax ];
+}
+
+const shoppingCart = [ phone, tablet];
+const tax = 0.15;
+
+const[total, taxTotal] = taxCalculation({
+    products: shoppingCart,
+    tax: tax
+})
+
+console.log('Total:', total);
+console.log('Tax:', tax);
+console.log('Total Tax:', taxTotal);
